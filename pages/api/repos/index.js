18c43ac82180a05954296a8aaa61470afbebe58b
jwhan <gksjeongw96@gmail.com>
2021-10-23 22:05:34 +0900

    Change scope in providers

--- pages/api/repos/index.js
+++ pages/api/repos/index.js
@@ -4,6 +4,9 @@ import { getSession } from "next-auth/client";
 export default async (req, res) => {
   const session = await getSession({ req });
   const octokit = new Octokit({ auth: session.accessToken });
-  const { data } = await octokit.request("/user/repos");
+  const { data } = await octokit.request("GET /user/repos", {
+    affiliation: "owner",
+    sort: "pushed",
+  });
   res.status(200).json(data);
 };
