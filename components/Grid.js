1a12bc536625c1d4f29eafbe2f002129ae9e22b1
jwhan <gksjeongw96@gmail.com>
2021-10-23 20:03:38 +0900

    Load my repos

--- components/Grid.js
+++ components/Grid.js
@@ -6,30 +6,28 @@ const Grid = () => {
   return (
     <div className="w-80 h-80 m-auto grid grid-cols-10 gap-1">
       {days.map((day, i) => (
-        <>
+        <div
+          key={i + 1}
+          className={
+            day
+              ? "bg-gray-500 rounded"
+              : "border-2 border-gray-500 hover:border-gray-200 hover:bg-gray-200 active:bg-gray-500 active:border-gray-500 border-rad rounded"
+          }
+        >
           <div
-            key={i + 1}
-            className={
-              day
-                ? "bg-gray-500"
-                : "border-2 border-gray-500 hover:border-gray-200 hover:bg-gray-200 active:bg-gray-500 active:border-gray-500"
-            }
+            className="w-full h-full"
+            data-tip="tooltip"
+            data-for={`day${i + 1}`}
+          ></div>
+          <ReactTooltip
+            id={`day${i + 1}`}
+            place="bottom"
+            type="dark"
+            effect="solid"
           >
-            <div
-              className="w-full h-full"
-              data-tip="tooltip"
-              data-for={`day${i + 1}`}
-            ></div>
-            <ReactTooltip
-              id={`day${i + 1}`}
-              place="bottom"
-              type="dark"
-              effect="solid"
-            >
-              <span>Day {i + 1}</span>
-            </ReactTooltip>
-          </div>
-        </>
+            <span>Day {i + 1}</span>
+          </ReactTooltip>
+        </div>
       ))}
     </div>
   );
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,33 +1,113 @@
 import { useSession } from "next-auth/client";
-import { useState } from "react";
-
+import React, { useState } from "react";
+import Modal from "react-modal";
 import Grid from "../components/Grid";
 
-export default function Dashboard() {
+const Dashboard = () => {
   const [session] = useSession();
 
   const [repos, setRepos] = useState([]);
+  const [myRepos, setMyRepos] = useState([]);
 
-  const fetchRepos = async () => {
-    const response = await fetch("/api/repos");
-    const data = await response.json();
-    setRepos(data);
+  const [loadReposModalIsOpen, setLoadReposModalIsOpen] = useState(false);
+  const [checkedRepos, setCheckedRepos] = useState([]);
+
+  const closeLoadReposModal = () => {
+    setCheckedRepos([]);
+    setLoadReposModalIsOpen(false);
+  };
+
+  const openLoadReposModal = () => {
+    const fetchRepos = async () => {
+      const response = await fetch("/api/repos");
+      const data = await response.json();
+      setRepos(data);
+      console.log(data);
+    };
+
+    fetchRepos();
+
+    setLoadReposModalIsOpen(true);
+  };
+
+  const handleCheckRepoChange = (e) => {
+    if (e.target.checked) {
+      let newCheckedRepos = [...checkedRepos];
+      newCheckedRepos.push(e.target.value);
+      setCheckedRepos(newCheckedRepos);
+    } else {
+      let newCheckedRepos = [...checkedRepos];
+      let filteredCheckedRepos = newCheckedRepos.filter(
+        (id) => id !== e.target.value
+      );
+      setCheckedRepos(filteredCheckedRepos);
+    }
+  };
+
+  const handleLoadReposSubmit = (e) => {
+    e.preventDefault();
+
+    let newCheckedReposObj = repos.filter((repo) =>
+      checkedRepos.includes(repo.id.toString())
+    );
+    console.log(newCheckedReposObj);
+    let newMyRepos = [...myRepos, ...newCheckedReposObj];
+    console.log(newMyRepos);
+
+    setMyRepos(newMyRepos);
+    setTimeout(() => {
+      closeLoadReposModal();
+    }, 500);
   };
 
   if (session) {
     return (
-      <>
-        <button onClick={fetchRepos}>Load repos</button>
-        {repos.map((repo) => {
-          return (
-            <div key={repo.id}>
-              {repo.id}: {repo.name}
-            </div>
-          );
-        })}
+      <div className="w-10/12 m-auto flex flex-row">
+        <div className="w-4/12">
+          <div>
+            <p>My repos</p>
+            <ul>
+              {myRepos.map((repo) => {
+                return <li key={repo.id}>{repo.name}</li>;
+              })}
+            </ul>
+          </div>
 
-        <Grid />
-      </>
+          <div>
+            <button onClick={openLoadReposModal}>Load repos</button>
+            <Modal
+              isOpen={loadReposModalIsOpen}
+              onRequestClose={closeLoadReposModal}
+              contentLabel="Load Repos"
+              ariaHideApp={false}
+            >
+              <h2>Repos in GitHub</h2>
+              <button onClick={closeLoadReposModal}>close</button>
+              <form onSubmit={handleLoadReposSubmit}>
+                {repos.map((repo) => {
+                  return (
+                    <div key={repo.id}>
+                      <label>
+                        <input
+                          name={repo.name}
+                          value={repo.id}
+                          type="checkbox"
+                          onChange={handleCheckRepoChange}
+                        ></input>
+                        {repo.name}
+                      </label>
+                    </div>
+                  );
+                })}
+                <input type="submit" value="Submit" />
+              </form>
+            </Modal>
+          </div>
+        </div>
+        <div>
+          <Grid />
+        </div>
+      </div>
     );
   }
   return (
@@ -35,4 +115,6 @@ export default function Dashboard() {
       <div>Please sign in</div>
     </>
   );
-}
+};
+
+export default Dashboard;
