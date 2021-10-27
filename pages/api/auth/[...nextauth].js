67bd42ba94aaf849f8550a3a24c805a9cb63071f
jwhan <gksjeongw96@gmail.com>
2021-10-27 18:54:21 +0900

    Update github access token while signing in

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,20 +1,12 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
-// import { MongoClient, ObjectId } from "mongodb";
 import Adapters from "next-auth/adapters";
-// import mongoose from "mongoose";
 
-// import clientPromise from "../../../lib/mongodb";
+import { getMongoose, updateGithubAccessToken } from "../../../lib/db";
 import models from "../../../models";
-// const Session = require("../../../models/session");
-
-// const client = new MongoClient(process.env.MONGODB_URL);
 
 export default async function auth(req, res) {
   return await NextAuth(req, res, {
-    jwt: {
-      encryption: true,
-    },
     providers: [
       Providers.GitHub({
         clientId: process.env.GITHUB_ID,
@@ -28,43 +20,15 @@ export default async function auth(req, res) {
       },
     }),
     database: process.env.MONGODB_URL,
-    // callbacks: {
-    //   session: async (session, token) => {
-    //     session.accessToken = token.githubAccessToken;
-    //     console.log("return session", session);
-    //     return session;
-
-    //     // mongoose
-    //     //   .connect(process.env.mONGODB_URL, {
-    //     //     useNewUrlParser: true,
-    //     //     useUnifiedTopology: true,
-    //     //   })
-    //     //   .then((result) => {
-    //     //     console.log("connected to db");
-    //     //     Session.findOne({ accessToken: session.accessToken })
-    //     //       .then((result) => {
-    //     //         console.log("found one", result);
-    //     //         session.userId = result.userId;
-    //     //         console.log(session.userId);
-    //     //         return session;
-    //     //       })
-    //     //       .catch((err) => console.log("err : ", err));
-    //     //   })
-    //     //   .catch((err) => {
-    //     //     console.log("cannot connect to db");
-    //     //     console.log(err);
-    //     //   });
-    //     // session.accessToken = token.githubAccessToken;
-    //   },
-    // },
-    // jwt: async (token, user, account, profile) => {
-    //   console.log("account: ", account);
-    //   if (user && account && account.provider === "github") {
-    //     token.username = profile.login;
-    //     token.githubAccessToken = account.accessToken;
-    //   }
-
-    //   return Promise.resolve(token);
-    // },
+    callbacks: {
+      signIn: async (user, account, profile) => {
+        const mongoose = await getMongoose();
+        await updateGithubAccessToken(user.id, account.accessToken);
+        return true;
+      },
+      session: async (session, token) => {
+        return session;
+      },
+    },
   });
 }
--- pages/api/repos/index.js
+++ pages/api/repos/index.js
@@ -1,7 +1,5 @@
 const { Octokit } = require("@octokit/core");
 import { getSession } from "next-auth/client";
-const Session = require("../../../models/session");
-const Account = require("../../../models/account");
 
 const { getMongoose, getGithubAccessToken } = require("../../../lib/db");
 
@@ -9,7 +7,6 @@ export default async (req, res) => {
   const session = await getSession({ req });
   const mongoose = await getMongoose();
   const githubAccessToken = await getGithubAccessToken(req);
-  console.log(githubAccessToken);
 
   const octokit = new Octokit({ auth: githubAccessToken });
   const { data } = await octokit.request("GET /user/repos", {
