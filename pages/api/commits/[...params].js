f67a80f8c21a81a924c4c9b81d7ea91ad7d1f4ed
jwhan <gksjeongw96@gmail.com>
2021-11-01 16:07:46 +0900

    List commits

---
+++ pages/api/commits/[...params].js
@@ -0,0 +1,24 @@
+const { Octokit } = require("@octokit/core");
+
+export default async (req, res) => {
+  const { params } = req.query;
+  const owner = params[0];
+  const repo = params[1];
+
+  const githubAccessToken = process.env.GITHUB_ACCESSTOKEN;
+
+  const octokit = new Octokit({ auth: githubAccessToken });
+  const { data } = await octokit.request("GET /repos/{owner}/{repo}/commits", {
+    owner: owner,
+    repo: repo,
+  });
+
+  let commits = data.filter((commit) => commit.committer.login === owner);
+  commits = commits.map((commit) => ({
+    html_url: commit.html_url,
+    date: commit.commit.author.date,
+    message: commit.commit.message,
+  }));
+
+  res.status(200).json(commits);
+};
